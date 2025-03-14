6c91a694126fdbcea7e762e571656d7b720b7ca1
Jonathan Hecl <jonathanhecl@gmail.com>
2025-03-13 23:23:37 -0300

    load file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jonathanhecl/sub-translation
 
 go 1.22.4
+
+require github.com/jonathanhecl/gollama v1.0.28
--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/jonathanhecl/gollama"
+)
+
+const (
+	FORMAT_SRT = "srt"
+	FORMAT_SSA = "ssa"
+)
 
 var (
 	version = "0.1.0"
@@ -8,4 +20,35 @@ var (
 
 func main() {
 	fmt.Println("Sub-Translation v" + version)
+
+	ctx := context.Background()
+	g := gollama.New("phi4")
+	g.Verbose = true
+	if err := g.PullIfMissing(ctx); err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	data, format, err := loadFile("red-en.srt")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println(string(data), format)
+}
+
+func loadFile(filename string) ([]byte, string, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, "", err
+	}
+
+	if strings.HasSuffix(filename, ".srt") {
+		return data, FORMAT_SRT, nil
+	} else if strings.HasSuffix(filename, ".ssa") {
+		return data, FORMAT_SSA, nil
+	}
+
+	return data, "", nil
 }
